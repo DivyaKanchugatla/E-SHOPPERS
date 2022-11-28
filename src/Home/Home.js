e7f936aca7afb1176ad0f919d7b930fd5c95d8c1
DIVYA KANCHUGATLA <divyakanchugatla5@gmail.com>
2022-11-28 17:23:22 +0530

    update

---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/MainNavbar/Navbar'
+import TopNavbar from '../components/TopNavbar/TopNavbar'
+
+
+const Home = () => {
+  return (
+    <div>
+        <TopNavbar/>
+        <Navbar/>
+        
+</div>
+  )
+}
+
+export default Home
