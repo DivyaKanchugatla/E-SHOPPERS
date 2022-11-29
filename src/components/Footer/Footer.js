c3d1577525397d3eef969fb17fb4001d9894b35b
ambikav98 <ambikav600@gamil.com>
2022-11-29 12:59:25 +0530

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,43 +10,43 @@ const Footer = () => {
                 <a href="shopper" class="footer-e-shop-head-container text-decoration-none">
                     <h1 class="mb-4 Shopper-header"><span class="E-heading">E</span>Shopper</h1>
                 </a>
-                <p>Dolore erat dolor sit lorem vero amet. Sed sit lorem magna, ipsum no sit erat lorem et magna ipsum dolore amet erat.</p>
-                <p class="mb-2"><i class="fas fa-map-marker-alt mr-3 icon-color"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fas fa-envelope mr-3 icon-color"></i>info@example.com</p>
-                <p class="mb-0"><i class="fas fa-phone-alt mr-3 icon-color"></i>+012 345 67890</p>
+                <p className='para'>Dolore erat dolor sit lorem vero amet. Sed sit lorem magna, ipsum no sit erat lorem et magna ipsum dolore amet erat.</p>
+                <p class="mb-2 footer-icon"><i class="fas fa-map-marker-alt mr-3 icon-color"></i>123 Street, New York, USA</p>
+                <p class="mb-2 footer-icon"><i class="fas fa-envelope mr-3 icon-color"></i>info@example.com</p>
+                <p class="mb-0 footer-icon"><i class="fas fa-phone-alt mr-3 icon-color"></i>+012 345 67890</p>
             </div>
             <div class="col-lg-8 col-md-12">
                 <div class="row">
                     <div class="col-md-4 mb-5">
                         <h5 class="quick-links-head mb-4">Quick Links</h5>
                         <div class="d-flex flex-column justify-content-start">
-                            <a class="text-dark mb-2" href="home"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                            <a class="text-dark mb-2" href="shop"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-                            <a class="text-dark mb-2" href="details"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-                            <a class="text-dark mb-2" href="cart"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-                            <a class="text-dark mb-2" href="checkout"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                            <a class="text-dark" href="contact"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+                            <a class="text-dark footer-link mb-2" href="home"><i class="fa fa-angle-right mr-2"></i>Home</a>
+                            <a class="text-dark footer-link mb-2" href="shop"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
+                            <a class="text-dark footer-link mb-2" href="details"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
+                            <a class="text-dark footer-link mb-2" href="cart"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
+                            <a class="text-dark footer-link mb-2" href="checkout"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
+                            <a class="text-dark footer-link" href="contact"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
                         </div>
                     </div>
                     <div class="col-md-4 mb-5">
                         <h5 class="quick-links-head mb-4">Quick Links</h5>
                         <div class="d-flex flex-column justify-content-start">
-                            <a class="text-dark mb-2" href="home1"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                            <a class="text-dark mb-2" href="shop1"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-                            <a class="text-dark mb-2" href="details1"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-                            <a class="text-dark mb-2" href="cart1"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-                            <a class="text-dark mb-2" href="checkout1"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                            <a class="text-dark" href="contact1"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+                            <a class="text-dark footer-link mb-2" href="home1"><i class="fa fa-angle-right mr-2"></i>Home</a>
+                            <a class="text-dark footer-link mb-2" href="shop1"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
+                            <a class="text-dark  footer-link mb-2" href="details1"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
+                            <a class="text-dark  footer-link mb-2" href="cart1"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
+                            <a class="text-dark  footer-link mb-2" href="checkout1"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
+                            <a class="text-dark footer-link" href="contact1"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
                         </div>
                     </div>
                     <div class="col-md-4 mb-5">
                         <h5 class="quick-links-head mb-4">Newsletter</h5>
                         <form action="">
                             <div class="form-group">
-                                <input type="text" class="form-control py-4" placeholder="Your Name" required="required" />
+                                <input type="text" class= "input-field " placeholder="Your Name" required="required" />
                             </div>
                             <div class="form-group">
-                                <input type="email" class="form-control py-4" placeholder="Your Email"
+                                <input type="email" class="input-field" placeholder="Your Email"
                                     required="required" />
                             </div>
                             <div>
--- src/components/Footer/OwlDemo.js
+++ src/components/Footer/OwlDemo.js
@@ -8,23 +8,19 @@ export class OwlDemo extends Component {
         {      
           return (  
               <div>  
-            <div class='container-fluid' >      
-            <div className="row title" style={{marginBottom: "20px"}} >      
-                 
-            </div>  
-        </div>  
+            
         <div class='container-fluid' >   
-        <OwlCarousel items={5} margin={8} autoplay ={true} >  
-        <div ><img  className="img shadow" alt = "pic1" src= {'https://technext.github.io/eshopper/img/vendor-5.jpg'}/></div>  
-           <div><img  className="img shadow" alt = "pic2" src= {'https://technext.github.io/eshopper/img/vendor-3.jpg'}/></div>  
-           <div><img  className="img shadow" alt = "pic3" src= {'https://technext.github.io/eshopper/img/vendor-2.jpg'}/></div>  
-           <div><img  className="img shadow" alt = "pic4" src= {'https://technext.github.io/eshopper/img/vendor-1.jpg'}/></div>  
-           <div><img className="img shadow" alt = "pic5" src= {'https://technext.github.io/eshopper/img/vendor-6.jpg'}/></div>  
-           <div><img className="img shadow" alt = "pic6" src= {'https://technext.github.io/eshopper/img/vendor-7.jpg'}/></div>  
-           <div><img className="img shadow" alt = "pic7" src= {'https://technext.github.io/eshopper/img/vendor-8.jpg'}/></div> 
-           <div><img  className="img shadow" alt = "pic8" src= {'https://technext.github.io/eshopper/img/vendor-3.jpg'}/></div>  
-           <div><img  className="img shadow" alt = "pic9" src= {'https://technext.github.io/eshopper/img/vendor-2.jpg'}/></div>  
-           <div><img  className="img shadow" alt = "pic10" src= {'https://technext.github.io/eshopper/img/vendor-1.jpg'}/></div> 
+        <OwlCarousel items={6} margin={3} autoplay ={true} >  
+        <div className='image-container'><img  className="img " alt = "pic1" src= {'https://technext.github.io/eshopper/img/vendor-5.jpg'}/></div>  
+           <div className='image-container'><img  className="img shadw" alt = "pic2" src= {'https://technext.github.io/eshopper/img/vendor-3.jpg'}/></div>  
+           <div className='image-container'><img  className="img shadw" alt = "pic3" src= {'https://technext.github.io/eshopper/img/vendor-2.jpg'}/></div>  
+           <div className='image-container'><img  className="img shaow" alt = "pic4" src= {'https://technext.github.io/eshopper/img/vendor-1.jpg'}/></div>  
+           <div className='image-container'><img className="img shaow" alt = "pic5" src= {'https://technext.github.io/eshopper/img/vendor-6.jpg'}/></div>  
+           <div className='image-container'><img className="img shaow" alt = "pic6" src= {'https://technext.github.io/eshopper/img/vendor-7.jpg'}/></div>  
+           <div className='image-container'><img className="img shdow" alt = "pic7" src= {'https://technext.github.io/eshopper/img/vendor-8.jpg'}/></div> 
+           <div className='image-container'><img  className="img shdow" alt = "pic8" src= {'https://technext.github.io/eshopper/img/vendor-3.jpg'}/></div>  
+           <div className='image-container'><img  className="img shdow" alt = "pic9" src= {'https://technext.github.io/eshopper/img/vendor-2.jpg'}/></div>  
+           <div className='image-container'><img  className="img shdow" alt = "pic10" src= {'https://technext.github.io/eshopper/img/vendor-1.jpg'}/></div> 
       </OwlCarousel>  
       </div>  
       </div>  
