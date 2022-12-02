451687754aa961fdaf363cb08ad0efc7391d7934
ambikav98 <ambikav600@gamil.com>
2022-12-02 11:45:04 +0530

    update

--- src/components/Footer/OwlDemo.js
+++ src/components/Footer/OwlDemo.js
@@ -8,9 +8,15 @@ export class OwlDemo extends Component {
         {      
           return (  
               <div>  
-            
+            <div class='container-fluid' >      
+            <div className="row title" style={{marginBottom: "20px"}} >      
+                 
+            </div>  
+        </div>  
         <div class='container-fluid' >   
-        <OwlCarousel items={6} margin={3} autoplay ={true} >  
+         <div className='row'>
+          <div className='d-none d-md-block'>
+          <OwlCarousel items={6} margin={3} autoplay ={true} >  
         <div className='image-container'><img  className="img " alt = "pic1" src= {'https://technext.github.io/eshopper/img/vendor-5.jpg'}/></div>  
            <div className='image-container'><img  className="img shadw" alt = "pic2" src= {'https://technext.github.io/eshopper/img/vendor-3.jpg'}/></div>  
            <div className='image-container'><img  className="img shadw" alt = "pic3" src= {'https://technext.github.io/eshopper/img/vendor-2.jpg'}/></div>  
@@ -21,7 +27,32 @@ export class OwlDemo extends Component {
            <div className='image-container'><img  className="img shdow" alt = "pic8" src= {'https://technext.github.io/eshopper/img/vendor-3.jpg'}/></div>  
            <div className='image-container'><img  className="img shdow" alt = "pic9" src= {'https://technext.github.io/eshopper/img/vendor-2.jpg'}/></div>  
            <div className='image-container'><img  className="img shdow" alt = "pic10" src= {'https://technext.github.io/eshopper/img/vendor-1.jpg'}/></div> 
-      </OwlCarousel>  
+           </OwlCarousel> 
+          </div>
+          </div> 
+      <div className='row'>
+         <div className='col-sm-12 d-sm-block d-md-none'>
+         <OwlCarousel items={2} margin={8} autoplay ={true} >  
+         <div className='image-container'><img  className="img " alt = "pic1" src= {'https://technext.github.io/eshopper/img/vendor-5.jpg'}/></div>  
+           <div className='image-container'><img  className="img shadw" alt = "pic2" src= {'https://technext.github.io/eshopper/img/vendor-3.jpg'}/></div>  
+           <div className='image-container'><img  className="img shadw" alt = "pic3" src= {'https://technext.github.io/eshopper/img/vendor-2.jpg'}/></div>  
+           <div className='image-container'><img  className="img shaow" alt = "pic4" src= {'https://technext.github.io/eshopper/img/vendor-1.jpg'}/></div>  
+           <div className='image-container'><img className="img shaow" alt = "pic5" src= {'https://technext.github.io/eshopper/img/vendor-6.jpg'}/></div>  
+           <div className='image-container'><img className="img shaow" alt = "pic6" src= {'https://technext.github.io/eshopper/img/vendor-7.jpg'}/></div>  
+           <div className='image-container'><img className="img shdow" alt = "pic7" src= {'https://technext.github.io/eshopper/img/vendor-8.jpg'}/></div> 
+           <div className='image-container'><img  className="img shdow" alt = "pic8" src= {'https://technext.github.io/eshopper/img/vendor-3.jpg'}/></div>  
+           <div className='image-container'><img  className="img shdow" alt = "pic9" src= {'https://technext.github.io/eshopper/img/vendor-2.jpg'}/></div>  
+           <div className='image-container'><img  className="img shdow" alt = "pic10" src= {'https://technext.github.io/eshopper/img/vendor-1.jpg'}/></div> 
+           
+
+          
+      
+       
+  
+       
+  </OwlCarousel> 
+         </div>
+      </div>
       </div>  
       </div>  
           )  
@@ -29,4 +60,4 @@ export class OwlDemo extends Component {
       }  
         
   
-export default OwlDemo;
+export default OwlDemo
